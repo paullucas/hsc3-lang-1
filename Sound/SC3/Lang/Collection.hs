cb4c271118dd1eb85c42773d12ee981773345311
Rohan Drape <rd@slavepianos.org>
2008-01-14 23:45:00 -0800

    Make exported module Sound.SC3.Lang.Collection
    darcs-hash:1a7d9ce8e8d5a5559e332e977a056449a9255a1d

---
+++ Sound/SC3/Lang/Collection.hs
@@ -0,0 +1,6 @@
+module Sound.SC3.Lang.Collection 
+    ( module Sound.SC3.Lang.Collection.Collection
+    , module Sound.SC3.Lang.Collection.SequenceableCollection ) where
+
+import Sound.SC3.Lang.Collection.Collection
+import Sound.SC3.Lang.Collection.SequenceableCollection
