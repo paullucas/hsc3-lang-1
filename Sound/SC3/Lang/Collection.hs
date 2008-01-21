6f292af5587353f2022f3a01d5e805c69fbd167c
Rohan Drape <rd@slavepianos.org>
2008-01-21 01:57:38 -0800

    Add module Sound.SC3.Lang.Collection.Numerical
    darcs-hash:61db9002c34427ce584290903cff99a95fc25fa1

--- Sound/SC3/Lang/Collection.hs
+++ Sound/SC3/Lang/Collection.hs
@@ -1,6 +1,8 @@
 module Sound.SC3.Lang.Collection 
     ( module Sound.SC3.Lang.Collection.Collection
+    , module Sound.SC3.Lang.Collection.Numerical
     , module Sound.SC3.Lang.Collection.SequenceableCollection ) where
 
 import Sound.SC3.Lang.Collection.Collection
+import Sound.SC3.Lang.Collection.Numerical
 import Sound.SC3.Lang.Collection.SequenceableCollection
---
+++ Sound/SC3/Lang/Collection/Numerical.hs
@@ -0,0 +1,37 @@
+module Sound.SC3.Lang.Collection.Numerical where
+
+import Sound.SC3.Lang.Collection.SequenceableCollection (zipWith_c)
+
+instance Num a => Num [a] where
+    negate         = map negate
+    (+)            = zipWith_c (+)
+    (-)            = zipWith_c (-)
+    (*)            = zipWith_c (*)
+    abs            = map abs
+    signum         = map signum
+    fromInteger n  = [fromInteger n]
+
+instance Fractional a => Fractional [a] where
+    recip          = map recip
+    (/)            = zipWith_c (/)
+    fromRational n = [fromRational n]
+
+instance Floating a => Floating [a] where
+    pi             = cycle [pi]
+    exp            = map exp
+    log            = map log
+    sqrt           = map sqrt
+    (**)           = zipWith_c (**)
+    logBase        = zipWith_c logBase
+    sin            = map sin
+    cos            = map cos
+    tan            = map tan
+    asin           = map asin
+    acos           = map acos
+    atan           = map atan
+    sinh           = map sinh
+    cosh           = map cosh
+    tanh           = map tanh
+    asinh          = map asinh
+    acosh          = map acosh
+    atanh          = map atanh
