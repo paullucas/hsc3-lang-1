29d08c4128515e9a21ad49b239461e0a80de6f45
Rohan Drape <rd@slavepianos.org>
2009-11-11 05:22:11 -0800

    delete unused files, do not re-export any patteern library
    Ignore-this: 82ca9fa77283d82e10d5db5bf99c83b7
    
    darcs-hash:11044fc66a5ab3415d861893143a4faafa107e0e

--- Sound/SC3/Lang.hs
+++ Sound/SC3/Lang.hs
@@ -1,8 +1,6 @@
 module Sound.SC3.Lang 
     ( module Sound.SC3.Lang.Collection
-    , module Sound.SC3.Lang.Math
-    , module Sound.SC3.Lang.Pattern ) where
+    , module Sound.SC3.Lang.Math ) where
 
 import Sound.SC3.Lang.Collection
 import Sound.SC3.Lang.Math
-import Sound.SC3.Lang.Pattern
