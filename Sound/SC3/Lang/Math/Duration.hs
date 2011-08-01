9d3e92bfdf986fb3c9e61a11725bcd683ccc1390
Rohan Drape <rd@slavepianos.org>
2011-08-01 07:25:06 -0700

    Math/Duration.hs
    Ignore-this: 5bd48c7db252bfb1d4d431f44dca6f8e
    
    darcs-hash:e01c744784ed4335db16a00f5e556cf2f936ea94

---
+++ Sound/SC3/Lang/Math/Duration.hs
@@ -0,0 +1,31 @@
+module Sound.SC3.Lang.Math.Duration where
+
+default_delta_f :: (Num a,Fractional a) => Duration a -> a
+default_delta_f d = dur d * stretch d * (60 / tempo d)
+
+default_sustain_f :: (Num a,Fractional a) => Duration a -> a
+default_sustain_f d = dur d * legato d * stretch d * (60 / tempo d)
+
+data Duration a = Duration {tempo :: a
+                           ,dur :: a
+                           ,stretch :: a
+                           ,legato :: a
+                           ,sustain_f :: Duration a -> a
+                           ,delta_f :: Duration a -> a
+                           ,lag :: a}
+
+delta :: Duration a -> a
+delta d = delta_f d d
+
+sustain :: Duration a -> a
+sustain d = sustain_f d d
+
+defaultDuration :: (Num a,Fractional a) => Duration a
+defaultDuration =
+    Duration {tempo = 60
+             ,dur = 1
+             ,stretch = 1
+             ,legato = 0.8
+             ,sustain_f = default_sustain_f
+             ,delta_f = default_delta_f
+             ,lag = 0.1}
