c00c93a1b30525f91d0b750d5b28365e2df2f1c4
Rohan Drape <rd@slavepianos.org>
2011-08-10 18:11:09 -0700

    Help/Pattern/arpeggio (pbind)
    Ignore-this: 5e10eb6240cabac821c0cc379eee6ee
    
    darcs-hash:71e9fe99de03c20d9c7b1ef42c99ed14adefe2fd

--- Help/Pattern/arpeggio.hs
+++ Help/Pattern/arpeggio.hs
@@ -1,8 +1,10 @@
+{-# Language OverloadedStrings #-}
 -- http://www.listarc.bham.ac.uk/lists/sc-users/msg07473.html
 
-import Data.List
+import Control.Monad
 import Sound.OpenSoundControl
 import Sound.SC3
+import Sound.SC3.Lang.Collection.Event
 import Sound.SC3.Lang.Pattern.List
 
 instr :: Synthdef
@@ -21,76 +23,66 @@ instr =
 	s = bLowPass i (c * f) r * 0.25
     in synthdef "analogarpeggio" (out o (pan2 (e * s * a) p 0.25))
 
-type R = Double
-type N = (R,R,R,R,R,R)
-
-note_msg :: N -> Int -> OSC
-note_msg (c,r,p,a,n,o) i =
-    let x = [("cutoffmult",c)
-            ,("res",r)
-            ,("pan",p)
-            ,("amp",a)
-            ,("freq",octCPS (o+(n/12)))]
-    in s_new "analogarpeggio" i AddToTail 1 x
-
-interp :: (Fractional t,Enum t) => t -> t -> t -> [t]
-interp n s e =
-    let i = (e - s) / n
-    in [s,s+i .. e]
-
-interp' :: (Fractional t,Enum t) => [t] -> [t] -> [t] -> [t]
-interp' n s e = concat (zipWith3 interp n s e)
-
-notes :: [N]
-notes =
-    let cutoffmult =
-            let n = pchoose 'a' [8,16,24,32]
-                s = pwhite 'b' 2.5 5.0
-                e = pwhite 'c' 1.5 7.0
-            in interp' n s e
-        res =
-            let n = pchoose 'd' [8,16,24,32]
-                s = pexprand 'e' 0.02 0.5
-                e = pexprand 'f' 0.02 1.0
-            in interp' n s e
-        pan =
-            let n = pchoose 'g' [8,16]
-                s = pwhite 'h' (-1.0) 1.0
-                e = pif (< 0) s (pwhite 'i' 0.0 1.0) (pwhite 'i' (-1.0) 0.0)
 {-
-                e = map (\i -> if i > 0
-                               then pwhite i 0.0 1.0
-                               else pwhite i (-1.0) 0.0) s
--}
-            in interp' n s e
-        amp =
-            let n = pchoose 'j' [8,16,24,32]
-                s = pchoose 'k' [0.25,0.05,0.5]
-                e = pchoose 'l' [0.25,0.05,0.5,0.01]
-            in interp' n s e
-        note =
-            let r = concat . replicate 8
-            in cycle (concat [r [10,6,1,-2]
-                             ,r [-3,1,6,9]
-                             ,r [9,6,1,-3]
-                             ,r [-2,1,6,10]
-                             ,r [8,6,1,-4]
-                             ,r [-3,1,6,9]
-                             ,r [8,6,1,-3]
-                             ,r [-6,1,6,8]])
-        octave = cycle (concatMap (replicate 8) [7,6,5,4,4,5,6,7])
-  in zip6 cutoffmult res pan amp note octave
+withSC3 (\fd -> async fd (d_recv instr))
 
-main :: IO ()
-main = do
-  let tempo = 157/60
-      dur = pwrand' 'n' [0.25,0.125,0.0625] [0.45,0.45,0.1]
-      dur' = pstutter 32 dur
-      run_note fd (i,n,d) = do
-            print ("run_note",i,d)
-            send fd (note_msg n i)
-            pauseThread (d / tempo)
-            send fd (n_set i [("gate",0)])
-      run fd = async fd (d_recv instr) >>
-               mapM_ (run_note fd) (zip3 (cycle [1024..2048]) notes dur')
-  withSC3 run
+:set -XOverloadedStrings
+
+audition (pbind [("instrument",pn (return "analogarpeggio") inf)
+                ,("dur",pwrand 'a' [0.1,0.25] [2/3,1/2] inf)
+                ,("freq",pwhite 'b' 440.0 1600.0 inf)
+                ,("pan",pwhite 'c' (-1.0) 1.0 inf)
+                ,("amp",pwhite 'd' 0.0 0.5 inf)
+                ,("cutoffmult",pwhite 'e' 4.0 5.0 inf)
+                ,("res",pwhite 'f' 0.0 1.0 inf)])
+
+-}
+
+pinterp :: Fractional a => Int -> a -> a -> P a
+pinterp n s e = pseries s ((e - s) / fromIntegral n) n
+
+pinterp' :: (Fractional a) => P Int -> P a -> P a -> P a
+pinterp' n s e = join (pzipWith3 pinterp n s e)
+
+notes :: [(Key,P Datum)]
+notes =
+    [("instrument",prepeat "analogarpeggio")
+    ,("dur"
+     ,let d = pwrand 'n' [0.25,0.125,0.0625] [0.45,0.45,0.1] inf
+      in pstutter 32 d)
+    ,("cutoffmult"
+     ,let n = prand 'a' [8,16,24,32] inf
+          s = pwhite 'b' 2.5 5.0 inf
+          e = pwhite 'c' 1.5 7.0 inf
+      in pinterp' n s e)
+    ,("res"
+     ,let n = prand 'd' [8,16,24,32] inf
+          s = pexprand 'e' 0.02 0.5 inf
+          e = pexprand 'f' 0.02 1.0 inf
+      in pinterp' n s e)
+    ,("pan"
+     ,let n = prand 'g' [8,16] inf
+          s = pwhite 'h' (-1.0) 1.0 inf
+          e = pif (fmap (< 0) s) (pwhite 'i' 0.0 1.0 inf) (pwhite 'i' (-1.0) 0.0 inf)
+      in pinterp' n s e)
+    ,("amp"
+     ,let n = prand 'j' [8,16,24,32] inf
+          s = prand 'k' [0.25,0.05,0.5] inf
+          e = prand 'l' [0.25,0.05,0.5,0.01] inf
+      in pinterp' n s e)
+    ,("note"
+     ,let f x = pn (fromList x) 8
+      in pseq (map f [[10,6,1,-2]
+                     ,[-3,1,6,9]
+                     ,[9,6,1,-3]
+                     ,[-2,1,6,10]
+                     ,[8,6,1,-4]
+                     ,[-3,1,6,9]
+                     ,[8,6,1,-3]
+                     ,[-6,1,6,8]]) inf)
+    ,("octave"
+     ,pstutter 8 (pseq [7,6,5,4,4,5,6,7] inf))]
+
+{-
+let tempo = 157 in audition (pedit "dur" (* (60/tempo)) (pbind notes))
+-}
