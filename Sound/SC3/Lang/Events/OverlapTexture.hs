80b1ab3fcf45bd52e9fc47b613743c861a4cf91b
Rohan Drape <rd@slavepianos.org>
2011-08-21 16:30:43 -0700

    OverlapTexture (pbind)
    Ignore-this: 49fe8bd752f477d71f9dae069e70d901
    
    darcs-hash:f497fa402890c87a430bd88a278fdf3ce1dc7a47

--- Sound/SC3/Lang/Events/OverlapTexture.hs
+++ Sound/SC3/Lang/Events/OverlapTexture.hs
@@ -1,21 +1,10 @@
 module Sound.SC3.Lang.Events.OverlapTexture where
 
+import Data.List
 import Sound.OpenSoundControl
 import Sound.SC3
-
-at' :: st -> Double -> ((st,Double) -> IO (Maybe (st,Double))) -> IO ()
-at' st t f = do
-  r <- f (st,t)
-  case r of
-    Just (st',t') -> do pauseThreadUntil (t + t')
-                        at' st' (t + t') f
-    Nothing -> return ()
-
-at :: st -> Double -> ((st,Double) -> IO (Maybe (st,Double))) -> IO ()
-at st t f = do
-  pauseThreadUntil t
-  _ <- at' st t f
-  return ()
+import Sound.SC3.Lang.Collection.Event
+import Sound.SC3.Lang.Pattern.List
 
 mk_env :: UGen -> UGen -> UGen
 mk_env a s =
@@ -31,14 +20,56 @@ with_env (a,s,_,_) g = with_env' g (constant a) (constant s)
 
 type OverlapTexture = (Double,Double,Double,Int)
 
-delay_time :: OverlapTexture -> Double
-delay_time (a,s,o,_) = (a + s + a) / o
+dur_legato :: OverlapTexture -> (Double,Double)
+dur_legato (a,s,o,_) = ((a + s + a) / o,o)
+
+gen_synth :: OverlapTexture -> UGen -> Synthdef
+gen_synth k g =
+  let n = show (hashUGen g)
+      g' = with_env k g
+  in synthdef n g'
+
+overlapTextureU' :: OverlapTexture -> UGen -> P (Event Double)
+overlapTextureU' k g =
+    let s = gen_synth k g
+        (d,l) = dur_legato k
+        (_,_,_,c) = k
+        i = return (Left s)
+    in pinstr i (pbind [("dur",pn (return d) c),("legato", return l)])
+
+overlapTextureU :: OverlapTexture -> UGen -> IO ()
+overlapTextureU k = audition . overlapTextureU' k
+
+overlapTextureS' :: OverlapTexture -> (st -> (UGen,st)) -> st -> P (Event Double)
+overlapTextureS' k u i_st =
+    let (d,l) = dur_legato k
+        (_,_,_,c) = k
+        g = take c (unfoldr (Just . u) i_st)
+        s = fromList (map (Left . gen_synth k) g)
+    in pinstr s (pbind [("dur",prepeat d),("legato",prepeat l)])
+
+overlapTextureS :: OverlapTexture -> (st -> (UGen,st)) -> st -> IO ()
+overlapTextureS k u = audition . overlapTextureS' k u
+
+at' :: st -> Double -> ((st,Double) -> IO (Maybe (st,Double))) -> IO ()
+at' st t f = do
+  r <- f (st,t)
+  case r of
+    Just (st',t') -> do pauseThreadUntil (t + t')
+                        at' st' (t + t') f
+    Nothing -> return ()
+
+at :: st -> Double -> ((st,Double) -> IO (Maybe (st,Double))) -> IO ()
+at st t f = do
+  pauseThreadUntil t
+  _ <- at' st t f
+  return ()
 
 overlapTextureM' :: Transport t => t -> OverlapTexture -> IO UGen -> IO ()
 overlapTextureM' fd k u = do
   t <- utcr
   let n = "ot_" ++ show t
-      dt = delay_time k
+      (dt,_) = dur_legato k
       (_,_,_,c) = k
       f (st,_) = do g <- u
                     let g' = with_env k g
@@ -50,33 +81,3 @@ overlapTextureM' fd k u = do
 overlapTextureM :: OverlapTexture -> IO UGen -> IO ()
 overlapTextureM k u = withSC3 (\fd -> overlapTextureM' fd k u)
 
-overlapTextureU' :: Transport t => t -> OverlapTexture -> UGen -> IO ()
-overlapTextureU' fd k g = do
-  t <- utcr
-  let n = "ot_" ++ show t
-      g' = with_env k g
-      dt = delay_time k
-      (_,_,_,c) = k
-      f (st,_) = do send fd (s_new n (-1) AddToTail 1 [])
-                    if st == 0 then return Nothing else return (Just (st-1,dt))
-  _ <- async fd (d_recv (synthdef n g'))
-  at c t f
-
-overlapTextureU :: OverlapTexture -> UGen -> IO ()
-overlapTextureU k u = withSC3 (\fd -> overlapTextureU' fd k u)
-
-overlapTextureS' :: Transport t => t -> OverlapTexture -> (st -> (UGen,st)) -> st -> IO ()
-overlapTextureS' fd k u i_st = do
-  t <- utcr
-  let n = "ot_" ++ show t
-      dt = delay_time k
-      (_,_,_,c) = k
-      f ((st,c'),_) = do let (g,st') = u st
-                             g' = with_env k g
-                         _ <- async fd (d_recv (synthdef n g'))
-                         send fd (s_new n (-1) AddToTail 1 [])
-                         if c' == 0 then return Nothing else return (Just ((st',c'-1),dt))
-  at (i_st,c) t f
-
-overlapTextureS :: OverlapTexture -> (st -> (UGen,st)) -> st -> IO ()
-overlapTextureS k u st = withSC3 (\fd -> overlapTextureS' fd k u st)
