4292d14aa609ebdc81c2ce08421ac9a2cc71328a
Rohan Drape <rd@slavepianos.org>
2011-08-21 16:29:13 -0700

    Instrument,defaultInstrument
    Ignore-this: d58fbdda7adf4b69a4ac8896a94a555a
    
    darcs-hash:3f46f011219bdf39ebcde755016585731026be87

--- Sound/SC3/Lang/Collection/Event.hs
+++ Sound/SC3/Lang/Collection/Event.hs
@@ -2,15 +2,16 @@ module Sound.SC3.Lang.Collection.Event where
 
 import qualified Data.Map as M
 import Data.Maybe
-import GHC.Exts (IsString(..))
+import Sound.SC3.ID
 import Sound.SC3.Lang.Math.Pitch
 
 type Key = String
 type Type = String
+type Instrument = Either Synthdef String
 data Event a = Event {e_type :: Type
                      ,e_id :: Int
+                     ,e_instrument :: Instrument
                      ,e_map :: M.Map Key a}
-               deriving (Show)
 
 e_lookup :: Key -> Event a -> Maybe a
 e_lookup k e = M.lookup k (e_map e)
@@ -28,7 +29,7 @@ e_lookup_f f k e =
       Just v' -> v'
 
 e_insert :: Key -> a -> Event a -> Event a
-e_insert k v (Event t n m) = Event t n (M.insert k v m)
+e_insert k v (Event t n i m) = Event t n i (M.insert k v m)
 
 e_lookup_m :: b -> (a -> b) -> Key -> Event a -> b
 e_lookup_m v f k e =
@@ -66,11 +67,11 @@ e_fwd e = e_lookup_v (e_dur e) "fwd" e * e_stretch e
 e_db :: Num a => Event a -> a
 e_db = e_lookup_v (-20) "db"
 
-dbAmp :: Floating a => a -> a
-dbAmp a = 10 ** (a * 0.05)
+dbAmp' :: Floating a => a -> a
+dbAmp' a = 10 ** (a * 0.05)
 
 e_amp :: Floating a => Event a -> a
-e_amp e = e_lookup_v (dbAmp (e_db e)) "amp" e
+e_amp e = e_lookup_v (dbAmp' (e_db e)) "amp" e
 
 e_legato :: Fractional a => Event a -> a
 e_legato = e_lookup_v 0.8 "legato"
@@ -82,12 +83,6 @@ e_sustain :: Fractional a => Event a -> a
 e_sustain =
     let f e = e_dur e * e_legato e * e_stretch e
     in e_lookup_f f "sustain"
-
-e_instrument :: IsString s => Event s -> s
-e_instrument = e_lookup_v (fromString "default") "instrument"
-
---e_instrument' :: Event -> String
---e_instrument' = datum_str' . e_instrument
 
 e_reserved :: [Key]
 e_reserved =
@@ -119,10 +114,36 @@ e_edit k f e =
 e_to_list :: Event a -> [(Key,a)]
 e_to_list = M.toList . e_map
 
-e_from_list :: Type -> Int -> [(Key,a)] -> Event a
-e_from_list t n = Event t n . M.fromList
+e_from_list :: Type -> Int -> Instrument -> [(Key,a)] -> Event a
+e_from_list t n i = Event t n i . M.fromList
+
+
+e_instrument_name :: Event a -> String
+e_instrument_name e =
+    case e_instrument e of
+      Left s -> synthdefName s
+      Right s -> s
+
+e_instrument_def :: Event a -> Maybe Synthdef
+e_instrument_def e =
+    case e_instrument e of
+      Left s -> Just s
+      Right _ -> Nothing
 
 e_unions :: [Event a] -> Event a
 e_unions e =
     let (t:_) = map e_type e
-    in Event t (-1) (M.unions (map e_map e))
+        (i:_) = map e_instrument e
+    in Event t (-1) i (M.unions (map e_map e))
+
+defaultInstrument :: Synthdef
+defaultInstrument =
+    let f = control KR "freq" 440
+        a = control KR "amp" 0.1
+        p = control KR "pan" 0
+        g = control KR "gate" 1
+        e = linen g 0.01 0.7 0.3 RemoveSynth
+        f3 = mce [f,f + rand 'a' (-0.4) 0,f + rand 'b' 0 0.4]
+        l = xLine KR (rand 'c' 4000 5000) (rand 'd' 2500 3200) 1 DoNothing
+        z = lpf (mix (varSaw AR f3 0 0.3 * 0.3)) l * e
+    in synthdef "default" (out 0 (pan2 z p a))
