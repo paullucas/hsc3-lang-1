18d011f21881fce5ac64bc6154c22cecc74075fe
Rohan Drape <rd@slavepianos.org>
2011-08-21 18:02:25 -0700

    xfadeTextureU
    Ignore-this: 4f9bab58f3f85bcaf8e0252a46011566
    
    darcs-hash:1e754424061d5afc230944d1a87a4394db468c47

--- Sound/SC3/Lang/Events/OverlapTexture.hs
+++ Sound/SC3/Lang/Events/OverlapTexture.hs
@@ -15,15 +15,26 @@ mk_env a s =
 with_env' :: UGen -> UGen -> UGen -> UGen
 with_env' g a s = out 0 (g * (mk_env a s))
 
-with_env :: OverlapTexture -> UGen -> UGen
-with_env (a,s,_,_) g = with_env' g (constant a) (constant s)
+with_env :: (Double,Double) -> UGen -> UGen
+with_env (a,s) g = with_env' g (constant a) (constant s)
 
 type OverlapTexture = (Double,Double,Double,Int)
 
-dur_legato :: OverlapTexture -> (Double,Double)
-dur_legato (a,s,o,_) = ((a + s + a) / o,o)
+overlapTexture_env :: OverlapTexture -> (Double,Double)
+overlapTexture_env (a,s,_,_) = (a,s)
 
-gen_synth :: OverlapTexture -> UGen -> Synthdef
+overlapTexture_dt :: OverlapTexture -> (Double,Double)
+overlapTexture_dt (a,s,o,_) = ((a + s + a) / o,o)
+
+type XFadeTexture = (Double,Double,Int)
+
+xfadeTexture_env :: XFadeTexture -> (Double,Double)
+xfadeTexture_env (a,s,_) = (a,s)
+
+xfadeTexture_dt :: XFadeTexture -> (Double,Double)
+xfadeTexture_dt (a,s,_) = let dt = a + s in (dt,(dt + a) / dt)
+
+gen_synth :: (Double,Double) -> UGen -> Synthdef
 gen_synth k g =
   let n = show (hashUGen g)
       g' = with_env k g
@@ -31,8 +42,8 @@ gen_synth k g =
 
 overlapTextureU' :: OverlapTexture -> UGen -> P (Event Double)
 overlapTextureU' k g =
-    let s = gen_synth k g
-        (d,l) = dur_legato k
+    let s = gen_synth (overlapTexture_env k) g
+        (d,l) = overlapTexture_dt k
         (_,_,_,c) = k
         i = return (Left s)
     in pinstr i (pbind [("dur",pn (return d) c),("legato", return l)])
@@ -40,12 +51,23 @@ overlapTextureU' k g =
 overlapTextureU :: OverlapTexture -> UGen -> IO ()
 overlapTextureU k = audition . overlapTextureU' k
 
+xfadeTextureU' :: XFadeTexture -> UGen -> P (Event Double)
+xfadeTextureU' k g =
+    let s = gen_synth (xfadeTexture_env k) g
+        (d,l) = xfadeTexture_dt k
+        (_,_,c) = k
+        i = return (Left s)
+    in pinstr i (pbind [("dur",pn (return d) c),("legato", return l)])
+
+xfadeTextureU :: XFadeTexture -> UGen -> IO ()
+xfadeTextureU k = audition . xfadeTextureU' k
+
 overlapTextureS' :: OverlapTexture -> (st -> (UGen,st)) -> st -> P (Event Double)
 overlapTextureS' k u i_st =
-    let (d,l) = dur_legato k
+    let (d,l) = overlapTexture_dt k
         (_,_,_,c) = k
         g = take c (unfoldr (Just . u) i_st)
-        s = fromList (map (Left . gen_synth k) g)
+        s = fromList (map (Left . gen_synth (overlapTexture_env k)) g)
     in pinstr s (pbind [("dur",prepeat d),("legato",prepeat l)])
 
 overlapTextureS :: OverlapTexture -> (st -> (UGen,st)) -> st -> IO ()
@@ -69,10 +91,10 @@ overlapTextureM' :: Transport t => t -> OverlapTexture -> IO UGen -> IO ()
 overlapTextureM' fd k u = do
   t <- utcr
   let n = "ot_" ++ show t
-      (dt,_) = dur_legato k
+      (dt,_) = overlapTexture_dt k
       (_,_,_,c) = k
       f (st,_) = do g <- u
-                    let g' = with_env k g
+                    let g' = with_env (overlapTexture_env k) g
                     _ <- async fd (d_recv (synthdef n g'))
                     send fd (s_new n (-1) AddToTail 1 [])
                     if st == 0 then return Nothing else return (Just (st-1,dt))
