2dfc15e5035b6c5f56addcc56e813dfac02ed5aa
Rohan Drape <rd@slavepianos.org>
2011-08-24 02:29:28 -0700

    Lang/Math/Random (MonadRandom)
    Ignore-this: 88be81349b5b56e51435c04f7491d26e
    
    darcs-hash:92953418f38e005cbd52246bb82f4ad2c5a7b9c4

---
+++ Sound/SC3/Lang/Math/Random.hs
@@ -0,0 +1,29 @@
+module Sound.SC3.Lang.Math.Random where
+
+import Control.Monad.Random {- MonadRandom -}
+
+rand :: (RandomGen g,Random n,Num n) => n -> Rand g n
+rand n = getRandomR (0,n)
+
+nrand :: (RandomGen g,Random n,Num n) => Int -> n -> Rand g [n]
+nrand k n = sequence (replicate k (rand n))
+
+rand2 :: (RandomGen g,Random n,Num n) => n -> Rand g n
+rand2 n = getRandomR (-n,n)
+
+nrand2 :: (RandomGen g,Random n,Num n) => Int -> n -> Rand g [n]
+nrand2 k n = sequence (replicate k (rand2 n))
+
+rrand :: (RandomGen g,Random n,Num n) => n -> n -> Rand g n
+rrand l r = getRandomR (l,r)
+
+nrrand :: (RandomGen g,Random n,Num n) => Int -> n -> n -> Rand g [n]
+nrrand k l r = sequence (replicate k (rrand l r))
+
+choose :: RandomGen g => [a] -> Rand g a
+choose l = do
+  i <- rand (length l - 1)
+  return (l !! i)
+
+nchoose :: (RandomGen g) => Int -> [a] -> Rand g [a]
+nchoose k l = sequence (replicate k (choose l))
--- Sound/SC3/Lang/Math/SimpleNumber.hs
+++ Sound/SC3/Lang/Math/SimpleNumber.hs
@@ -16,14 +16,37 @@ exprand l r = rrand 0.0 1.0 >>= return . exprandrng l r
 rrand' :: (Random n, RandomGen g) => n -> n -> g -> (n,g)
 rrand' = curry randomR
 
+nrrand' :: (RandomGen g,Random a,Num a) => Int -> a -> a -> g -> ([a],g)
+nrrand' n l r =
+    let go x 0 g = (x,g)
+        go x k g =
+            let (y,g') = randomR (l,r) g
+            in go (y:x) (k - 1) g'
+    in go [] n
+
+nrrand :: (Random a, Num a) => Int -> a -> a -> IO [a]
+nrrand n l = getStdRandom . nrrand' n l
+
 rrand :: (Random n) => n -> n -> IO n
 rrand = curry randomRIO
+
+rand' :: (RandomGen g,Random n,Num n) => n -> g -> (n,g)
+rand' n = randomR (0,n)
 
 rand :: (Random n,Num n) => n -> IO n
 rand n = randomRIO (0,n)
 
+rand2' :: (RandomGen g,Random n,Num n) => n -> g -> (n,g)
+rand2' n = randomR (-n,n)
+
 rand2 :: (Random n,Num n) => n -> IO n
 rand2 n = randomRIO (-n,n)
+
+nrand2' :: (RandomGen g,Random a,Num a) => Int -> a -> g -> ([a],g)
+nrand2' n i = nrrand' n 0 i
+
+nrand2 :: (Random a, Num a) => Int -> a -> IO [a]
+nrand2 n = getStdRandom . nrand2' n
 
 coin' :: (RandomGen g, Random a, Ord a, Fractional a) => a -> g -> (Bool,g)
 coin' n g =
