0d3e8fd432119cb9b13fe55ba980d0d3a75f9532
Rohan Drape <rd@slavepianos.org>
2012-02-29 18:32:09 -0800

    +Control/Midi:Control_Message
    Ignore-this: 1cdf46155da44cab587c489a62b6812b
    
    darcs-hash:294f8b387224bc00ad1652d4776b2462f9e398ae

--- Sound/SC3/Lang/Control/Midi.hs
+++ Sound/SC3/Lang/Control/Midi.hs
@@ -7,28 +7,87 @@ module Sound.SC3.Lang.Control.Midi where
 
 import qualified Control.Exception as E
 import "mtl" Control.Monad.State
+import Data.Bits
 import qualified Data.ByteString.Lazy as B {- bytestring -}
 import qualified Data.Map as M {- containers -}
 import Sound.OpenSoundControl {- hosc -}
 
 -- | <http://www.midi.org/techspecs/midimessages.php>
-data Midi_Message a = All_Notes_Off a
-                    | All_Sound_Off a
-                    | Chanel_Aftertouch a a
+data Midi_Message a = Chanel_Aftertouch a a
                     | Control_Change a a a
-                    | Local_Control a a
-                    | Mono_Mode_On a a
                     | Note_On a a a
                     | Note_Off a a a
-                    | Omni_Mode_Off a
-                    | Omni_Mode_On a
-                    | Poly_Mode_On a
                     | Polyphic_Key_Pressure a a a
                     | Program_Change a a
                     | Pitch_Bend a a
-                    | Reset_All_Controllers a a
                     | Unknown [a]
                       deriving (Eq,Show)
+
+-- | 'Control_Change' midi messages have, in some cases, commonly
+-- defined meanings.
+data Control_Message a = All_Notes_Off a
+                       | All_Sound_Off a
+                       | Balance a a
+                       | Bank_Select a a
+                       | Breath_Controller a a
+                       | Expression_Controller a a
+                       | Foot_Controller a a
+                       | Local_Control a a
+                       | Modulation_Wheel a a
+                       | Mono_Mode_On a a
+                       | Omni_Mode_Off a
+                       | Omni_Mode_On a
+                       | Pan a a
+                       | Poly_Mode_On a
+                       | Portamento_On_Off a a
+                       | Portamento_Time a a
+                       | Reset_All_Controllers a a
+                       | Soft_Pedal_On_Off a a
+                       | Sostenuto_On_Off a a
+                       | Sustain_On_Off a a
+                       | Undefined
+                         deriving (Eq,Show)
+
+-- | 'Control_Change' midi messages may, in some cases, have commonly
+-- defined meanings.
+--
+-- > control_message (0,123,0) == All_Notes_Off 0
+control_message :: Num a => (a,a,a) -> Control_Message a
+control_message (i,j,k) =
+    case j of
+      0 -> Bank_Select i k
+      1 -> Modulation_Wheel i k
+      2 -> Breath_Controller i k
+      4 -> Foot_Controller i k
+      5 -> Portamento_Time i k
+      8 -> Balance i k
+      10 -> Pan i k
+      11 -> Expression_Controller i k
+      64 -> Sustain_On_Off i k
+      65 -> Portamento_On_Off i k
+      66 -> Sostenuto_On_Off i k
+      67 -> Soft_Pedal_On_Off i k
+      120 -> All_Sound_Off i
+      121 -> Reset_All_Controllers i k
+      122 -> Local_Control i j
+      123 -> All_Notes_Off i
+      124 -> Omni_Mode_Off i
+      125 -> Omni_Mode_On i
+      126 -> Mono_Mode_On i j
+      127 -> Poly_Mode_On i
+      _ -> Undefined
+
+-- | Join two 7-bit values into a 14-bit value.
+--
+-- > map (uncurry b_join) [(0,0),(0,64),(127,127)] == [0,8192,16383]
+b_join :: Bits a => a -> a -> a
+b_join p q = p .|. shiftL q 7
+
+-- | Inverse of 'b_join'.
+--
+-- > map b_sep [0,8192,16383] == [(0,0),(0,64),(127,127)]
+b_sep :: Bits t => t -> (t, t)
+b_sep n = (0x7f .&. n,0xff .&. shiftR n 7)
 
 -- | Parse @midi-osc@ @/midi/@ message.
 parse_b :: Integral n => OSC -> [n]
@@ -45,25 +104,17 @@ parse_c m =
       _ -> []
 
 -- | Variant of 'parse_c' that constructs a 'Midi_Message'.
-parse_m :: Integral n => OSC -> Midi_Message n
+parse_m :: (Bits n,Integral n) => OSC -> Midi_Message n
 parse_m m =
     case parse_c m of
       [0x8,i,j,k] -> Note_Off i j k
       [0x9,i,j,0] -> Note_Off i j 0
       [0x9,i,j,k] -> Note_On i j k
-      [0xA,i,j,k] -> Polyphic_Key_Pressure i j k
-      [0xB,i,120,0] -> All_Sound_Off i
-      [0xB,i,121,j] -> Reset_All_Controllers i j
-      [0xB,i,122,j] -> Local_Control i j
-      [0xB,i,123,0] -> All_Notes_Off i
-      [0xB,i,124,0] -> Omni_Mode_Off i
-      [0xB,i,125,0] -> Omni_Mode_On i
-      [0xB,i,126,j] -> Mono_Mode_On i j
-      [0xB,i,127,0] -> Poly_Mode_On i
-      [0xB,i,j,k] -> Control_Change i j k
-      [0xC,i,j] -> Program_Change i j
-      [0xD,i,j] -> Chanel_Aftertouch i j
-      [0xE,i,j] -> Pitch_Bend i j
+      [0xa,i,j,k] -> Polyphic_Key_Pressure i j k
+      [0xb,i,j,k] -> Control_Change i j k
+      [0xc,i,j] -> Program_Change i j
+      [0xd,i,j] -> Chanel_Aftertouch i j
+      [0xe,i,j,k] -> Pitch_Bend i (b_join j k)
       x -> Unknown x
 
 -- | @SC3@ node identifiers are integers.
@@ -120,7 +171,7 @@ start_midi receiver = do
   send m_fd (Message "/receive" [Int 0xffff])
   let step = liftIO (recv m_fd) >>=
              midi_act (receiver s_fd)
-      ex e = print ("midi_run:",show (e::E.AsyncException)) >>
+      ex e = print ("start_midi",show (e::E.AsyncException)) >>
              close m_fd >>
              close s_fd
       runs = runStateT (repeatM_ step) (k_init 1000) >>
