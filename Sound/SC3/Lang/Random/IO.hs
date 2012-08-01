f66ea5a53280b226c932b7eab56898226016c4c0
Rohan Drape <rd@slavepianos.org>
2012-07-31 22:26:14 -0700

    ~Random/IO (MonadIO m)
    Ignore-this: 1d90ee7bab8c47ea515ae4aa99458358
    
    darcs-hash:f6d705c0a981eb82d1ef64284bc41bca567b0dd5

--- Sound/SC3/Lang/Random/IO.hs
+++ Sound/SC3/Lang/Random/IO.hs
@@ -1,49 +1,56 @@
 -- | 'getStdRandom' based @sclang@ random number functions.
 module Sound.SC3.Lang.Random.IO where
 
+import Control.Monad.IO.Class
 import Sound.SC3.Lang.Random.Gen as R
 import System.Random {- random -}
 
+randomM :: (Random a, MonadIO m) => (a, a) -> m a
+randomM = liftIO . randomRIO
+
 -- | @SimpleNumber.rand@ is 'randomRIO' in (0,/n/).
-rand :: (Random n,Num n) => n -> IO n
-rand n = randomRIO (0,n)
+rand :: (MonadIO m,Random n,Num n) => n -> m n
+rand n = randomM (0,n)
 
 -- | @SimpleNumber.rand2@ is 'randomRIO' in (-/n/,/n/).
-rand2 :: (Random n,Num n) => n -> IO n
-rand2 n = randomRIO (-n,n)
+rand2 :: (MonadIO m,Random n,Num n) => n -> m n
+rand2 n = randomM (-n,n)
+
+randomG :: MonadIO m => (StdGen -> (a, StdGen)) -> m a
+randomG = liftIO . getStdRandom
 
 -- | Variant of 'rand2' generating /k/ values.
 nrand2 :: (Random a, Num a) => Int -> a -> IO [a]
-nrand2 n = getStdRandom . R.nrand2 n
+nrand2 k = randomG . R.nrand2 k
 
 -- | @SimpleNumber.rrand@ is 'curry' 'randomRIO'.
-rrand :: (Random n) => n -> n -> IO n
-rrand = curry randomRIO
+rrand :: (MonadIO m,Random n) => n -> n -> m n
+rrand l r = randomM (l,r)
 
 -- | Variant of 'rrand' generating /k/ values.
-nrrand :: (Random a, Num a) => Int -> a -> a -> IO [a]
-nrrand n l = getStdRandom . R.nrrand n l
+nrrand :: (MonadIO m,Random a, Num a) => Int -> a -> a -> m [a]
+nrrand k l = randomG . R.nrrand k l
 
 -- | @SequenceableCollection.choose@ selects an element at random.
-choose :: [a] -> IO a
-choose = getStdRandom . R.choose
+choose :: MonadIO m => [a] -> m a
+choose = randomG . R.choose
 
 -- | @SimpleNumber.exprand@ generates exponentially distributed random
 -- number in the given interval.
-exprand :: (Floating n,Random n) => n -> n -> IO n
-exprand l = getStdRandom . R.exprand l
+exprand :: (MonadIO m,Floating n,Random n) => n -> n -> m n
+exprand l = randomG . R.exprand l
 
 -- | @SimpleNumber.coin@ is 'True' at given probability, which is in
 -- range (0,1).
-coin :: (Random n,Fractional n,Ord n) => n -> IO Bool
-coin = getStdRandom . R.coin
+coin :: (MonadIO m,Random n,Fractional n,Ord n) => n -> m Bool
+coin = randomG . R.coin
 
 -- | @List.scramble@ shuffles the elements.
-scramble :: [t] -> IO [t]
-scramble = getStdRandom . R.scramble
+scramble :: MonadIO m => [t] -> m [t]
+scramble = randomG . R.scramble
 
 -- | @SequenceableCollection.wchoose@ selects an element from a list
 -- given a list of weights which sum to @1@.
-wchoose :: (Random a,Ord a,Fractional a) => [b] -> [a] -> IO b
-wchoose l = getStdRandom . R.wchoose l
+wchoose :: (MonadIO m,Random a,Ord a,Fractional a) => [b] -> [a] -> m b
+wchoose l = randomG . R.wchoose l
 
