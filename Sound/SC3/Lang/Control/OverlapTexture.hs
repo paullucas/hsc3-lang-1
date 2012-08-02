68bdce68d12a9d91ecf18244bbbce83e0f1bedf9
Rohan Drape <rd@slavepianos.org>
2012-08-02 06:25:33 -0700

    ~OverlapTexture (re-order as SC2)
    Ignore-this: 133a1605a26cae9dc33fb1cde3ee18e5
    
    darcs-hash:a767d3afc9e3a4dba6dbd2981f132329b4c4c93b

--- Sound/SC3/Lang/Control/OverlapTexture.hs
+++ Sound/SC3/Lang/Control/OverlapTexture.hs
@@ -14,11 +14,11 @@ import Sound.SC3.Lang.Control.Instrument
 import Sound.SC3.Lang.Pattern.ID
 
 -- | Make an 'envGen' 'UGen' with 'envLinen'' structure with given
--- /attack/\//delay/ and /sustain/ times.
+-- /sustain/ and /transition/ times.
 mk_env :: UGen -> UGen -> UGen
-mk_env a s =
+mk_env s t =
     let c = EnvNum 4
-        p = envLinen' a s a 1 (c,c,c)
+        p = envLinen' t s t 1 (c,c,c)
     in envGen KR 1 1 0 1 RemoveSynth p
 
 -- | Apply 'mk_env' envelope to input signal and write to output bus @0@.
@@ -28,35 +28,49 @@ with_env' g a = out 0 . (*) g . mk_env a
 -- | Variant of 'with_env'' where envelope parameters are lifted from
 -- 'Double' to 'UGen'.
 with_env :: (Double,Double) -> UGen -> UGen
-with_env (a,s) g = with_env' g (constant a) (constant s)
+with_env (s,t) g = with_env' g (constant s) (constant t)
 
 -- | Control parameters for 'overlapTextureU' and related functions.
--- Components are: 1. attack time, 2. sustain time, 3. number of
+-- Components are: 1. sustain time, 2. transition time, 3. number of
 -- overlaping (simultaneous) nodes and 4. number of nodes altogether.
 type OverlapTexture = (Double,Double,Double,Int)
 
--- | Extract envelope parameters for 'with_env' from 'OverlapTexture'.
-overlapTexture_env :: OverlapTexture -> (Double,Double)
-overlapTexture_env (a,s,_,_) = (a,s)
+data OverlapTexture_ =
+    OverlapTexture {sustain_time :: Double
+                   ,transition_time :: Double
+                   ,overlaps :: Double
+                   ,max_nodes :: Int}
 
--- | Extract /duration/ and /legato/ paramaters from 'OverlapTexture'.
+-- | Extract envelope parameters (sustain and transition times) for
+-- 'with_env' from 'OverlapTexture'.
+overlapTexture_env :: OverlapTexture -> (Double,Double)
+overlapTexture_env (s,t,_,_) = (s,t)
+
+-- | (/legato/,/duration/) parameters. The /duration/ is the
+-- inter-offset time, /legato/ is the scalar giving the sounding time
+-- in relation to the inter-offset time.
+type Texture_DT = (Double,Double)
+
+-- | Extract /legato/ (duration of sound proportional to inter-offset
+-- time) and /duration/ (inter-offset time) parameters from
+-- 'OverlapTexture'.
 --
--- > overlapTexture_dt (1,3,5,undefined) == (1,5)
-overlapTexture_dt :: OverlapTexture -> (Double,Double)
-overlapTexture_dt (a,s,o,_) = ((a + s + a) / o,o)
+-- > overlapTexture_dt (3,1,5,maxBound) == (5,1)
+overlapTexture_dt :: OverlapTexture -> Texture_DT
+overlapTexture_dt (s,t,o,_) = (o,(t + s + t) / o)
 
 -- | Control parameters for 'xfadeTextureU' and related functions.
--- Components are: 1. attack time, 2. sustain time, 3. number of nodes
--- instatiated altogether.
+-- Components are: 1. sustain time, 2. transition time, 3. number of
+-- nodes instatiated altogether.
 type XFadeTexture = (Double,Double,Int)
 
 -- | Extract envelope parameters for 'with_env' from 'XFadeTexture'.
 xfadeTexture_env :: XFadeTexture -> (Double,Double)
-xfadeTexture_env (a,s,_) = (a,s)
+xfadeTexture_env (s,t,_) = (s,t)
 
--- | Extract /duration/ and /legato/ paramaters from 'XFadeTexture'.
-xfadeTexture_dt :: XFadeTexture -> (Double,Double)
-xfadeTexture_dt (a,s,_) = let dt = a + s in (dt,(dt + a) / dt)
+-- | Extract /legato/ and /duration/ paramaters from 'XFadeTexture'.
+xfadeTexture_dt :: XFadeTexture -> Texture_DT
+xfadeTexture_dt (s,t,_) = let r = t + s in ((r + t) / r,r)
 
 -- | Generate 'Synthdef' from envelope parameters for 'with_env' and
 -- a continuous signal.
@@ -71,7 +85,7 @@ gen_synth k g =
 overlapTextureU' :: OverlapTexture -> UGen -> P Event
 overlapTextureU' k g =
     let s = gen_synth (overlapTexture_env k) g
-        (d,l) = overlapTexture_dt k
+        (l,d) = overlapTexture_dt k
         (_,_,_,c) = k
         i = return (InstrumentDef s False)
     in pinstr i (pbind [("dur",pn (return d) c),("legato", return l)])
@@ -83,7 +97,7 @@ overlapTextureU' k g =
 -- >
 -- > let {o = sinOsc AR (rand 'α' 440 880) 0
 -- >     ;u = pan2 o (rand 'β' (-1) 1) (rand 'γ' 0.1 0.2)}
--- > in overlapTextureU (1,3,6,maxBound) u
+-- > in overlapTextureU (3,1,6,9) u
 overlapTextureU :: OverlapTexture -> UGen -> IO ()
 overlapTextureU k = audition . overlapTextureU' k
 
@@ -118,7 +132,7 @@ overlapTextureU_pp k u nc f = do
 xfadeTextureU' :: XFadeTexture -> UGen -> P Event
 xfadeTextureU' k g =
     let s = gen_synth (xfadeTexture_env k) g
-        (d,l) = xfadeTexture_dt k
+        (l,d) = xfadeTexture_dt k
         (_,_,c) = k
         i = return (InstrumentDef s False)
     in pinstr i (pbind [("dur",pn (return d) c),("legato", return l)])
@@ -127,7 +141,7 @@ xfadeTextureU' k g =
 --
 -- > let {o = sinOsc AR (rand 'α' 440 880) 0
 -- >     ;u = pan2 o (rand 'β' (-1) 1) (rand 'γ' 0.1 0.2)}
--- > in xfadeTextureU (1,1,6) u
+-- > in xfadeTextureU (1,3,6) u
 xfadeTextureU :: XFadeTexture -> UGen -> IO ()
 xfadeTextureU k = audition . xfadeTextureU' k
 
@@ -145,7 +159,7 @@ type STF st = (st -> (UGen,st))
 -- given initial state.
 overlapTextureS' :: OverlapTexture -> STF st -> st -> P Event
 overlapTextureS' k u i_st =
-    let (d,l) = overlapTexture_dt k
+    let (l,d) = overlapTexture_dt k
         (_,_,_,c) = k
         g = take c (unfoldr (Just . u) i_st)
         i = flip InstrumentDef False
@@ -188,7 +202,7 @@ overlapTextureM' :: Transport m => OverlapTexture -> m UGen -> m ()
 overlapTextureM' k u = do
   t <- utcr
   let n = "ot_" ++ show t
-      (dt,_) = overlapTexture_dt k
+      (_,dt) = overlapTexture_dt k
       (_,_,_,c) = k
       f (st,_) = do g <- u
                     let g' = with_env (overlapTexture_env k) g
