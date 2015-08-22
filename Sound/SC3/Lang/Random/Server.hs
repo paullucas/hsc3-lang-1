e74c632979090b22ab9d0edb8850d17ddec8b4f9
Rohan Drape <rd@slavepianos.org>
2015-08-21 18:10:56 -0700

    +Sound.SC3.Lang.Random.Server
    Ignore-this: fc1d23f2ccfad3fc6da4a80a64daaec6
    
    darcs-hash:83a8748d5527e239e642710a02be0538aee08445

---
+++ Sound/SC3/Lang/Random/Server.hs
@@ -0,0 +1,14 @@
+module Sound.SC3.Lang.Random.Server where
+
+import Sound.OSC {- hosc -}
+import Sound.SC3 {- hsc3 -}
+
+import qualified Sound.SC3.Lang.Random.IO as L {- hsc3-lang -}
+
+-- | Get current data from buffer, scramble it, and write it back.
+buf_scramble :: (MonadIO m, DuplexOSC m) => Int -> m ()
+buf_scramble n = do
+  (_,sz,_,_) <- b_query1_unpack n
+  b <- b_getn1_data n (0,sz - 1)
+  r <- liftIO (L.scramble b)
+  send (b_setn1 n 0 r)
